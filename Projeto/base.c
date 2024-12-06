ff8765c308dcd033fcdf85fad28330e6b1cf04bf
MrMagmaPT <pmrmagma@gmail.com>
2024-12-06 11:53:31 +0000

    A Preparar tuddo para comecar a tortura

---
+++ Projeto/base.c
@@ -0,0 +1,8 @@
+//bibliotecas
+#include<stdlib.h> // 1 Permite usar o  system("cls"); (limpar o ecr�)   fflush(stdin);  (limpar o buffer do teclado)
+#include<stdio.h>  // 2 Normal msm input and output
+#include<conio.h>  // 4 Coloquei este aqui por causa do getch (� para que o executavel n feche automaticamente ) 
+#include<locale.h> // 5 Para poder por uma lingua de um local permitindo a utiliza��o de certos tipos de carcteres como por exemplo "�"
+#include<ctype.h>  // 6 Para faser o simbolo do € de forma corretinha ;3
+#include<string.h> // 7 Para fazer cenas com strings
+#include<time.h> // 7 Para fazer cenas com strings
